a4eb885ba187ebb253de99198a7099212baae80a
BkJonathan <admin@sinphyukyun.com>
2020-01-16 17:02:50 +0630

    Add Product List

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css"
+
 new Vue({
   router,
   store,
--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,17 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const testData = [];
+for (let i = 1; i <= 10; i++) { testData.push({
+  id: i, name: `Product #${i}`, category: `Category ${i % 3}`,
+  description: `This is Product #${i}`, price: i * 50 })
+}
+
 export default new Vuex.Store({
-  state: {},
+  strict: true,
+  state: {
+    products: testData
+  },
   mutations: {},
   actions: {},
   modules: {}
