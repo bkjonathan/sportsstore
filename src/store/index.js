cbb4329947af60dc65e1c80938e32e636fbcf9b5
BkJonathan <admin@sinphyukyun.com>
2020-01-16 17:07:41 +0630

    Adding Product Pagination

--- src/store/index.js
+++ src/store/index.js
@@ -4,17 +4,40 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 const testData = [];
-for (let i = 1; i <= 10; i++) { testData.push({
-  id: i, name: `Product #${i}`, category: `Category ${i % 3}`,
-  description: `This is Product #${i}`, price: i * 50 })
+for (let i = 1; i <= 10; i++) {
+  testData.push({
+    id: i,
+    name: `Product #${i}`,
+    category: `Category ${i % 3}`,
+    description: `This is Product #${i}`,
+    price: i * 50
+  });
 }
 
 export default new Vuex.Store({
   strict: true,
   state: {
-    products: testData
+    products: testData,
+    productsTotal: testData.length,
+    currentPage: 1,
+    pageSize: 4
   },
-  mutations: {},
+  getters: {
+    processedProducts: state => {
+      let index = (state.currentPage - 1) * state.pageSize;
+      return state.products.slice(index, index + state.pageSize);
+    },
+    pageCount: state => Math.ceil(state.productsTotal / state.pageSize)
+  },
+  mutations: {
+    setCurrentPage(state, page) {
+      state.currentPage = page;
+    },
+    setPageSize(state, size) {
+      state.pageSize = size;
+      state.currentPage = 1;
+    }
+  },
   actions: {},
   modules: {}
 });
