f48ed910eb31b4bfc4ffe0f76a2648077615d3ac
BkJonathan <admin@sinphyukyun.com>
2020-01-17 00:07:21 +0630

    Creating a Global Filter

--- src/main.js
+++ src/main.js
@@ -7,7 +7,13 @@ import store from "./store";
 Vue.config.productionTip = false;
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css"
+import "font-awesome/css/font-awesome.min.css";
+
+Vue.filter("currency", value =>
+  new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(
+    value
+  )
+);
 
 new Vue({
   router,
